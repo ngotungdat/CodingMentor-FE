11b1bb9113d6f286df4eacfc0cfd6ffda0409219
tamchamhai <ngophilongspkt@gmail.com>
2022-05-06 17:56:56 +0700

    fix bug

--- src/components/Global/CourseList/CourseList.tsx
+++ src/components/Global/CourseList/CourseList.tsx
@@ -152,7 +152,7 @@ const CourseList = () => {
 			status: true
 		})
 		try {
-			const res = await teacherApi.getAll({ pageSize: 99999 })
+			const res = await teacherApi.getAll({ pageSize: 99999,StatusID: 0, pageIndex:1 })
 			if (res.status === 200) {
 				const newTeacherList = [{ title: '---Trống---', value: 0 }, ...fmSelectArr(res.data.data, 'FullNameUnicode', 'UserInformationID')]
 				setOptionListForUpdate({

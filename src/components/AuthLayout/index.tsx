6c3f75c497e91bcae22a5821d779052b08d52393
Châu Chất Chơi <soc900k@gmail.com>
2022-05-06 09:54:21 +0700

    Update: Login Background

--- src/components/AuthLayout/index.tsx
+++ src/components/AuthLayout/index.tsx
@@ -73,41 +73,8 @@ function AuthLayout({ children }) {
 
 			<div className={styles.wrapper}>
 				<div className={styles['image-wrapper']}>
-					<div className={styles['calendar-wrapper']}>
-						<div className={styles.calendar}>
-							{dateState?.hour !== undefined ? (
-								<>
-									{/* <p className={styles.month_year}>{dateState.date}</p>
-									<p className={styles.time}>
-										{dateState.month} {dateState.year}
-									</p>
-									<p className={styles.month_year}>
-										{dateState.hour} : {dateState.minute} : {dateState.second}
-									</p> */}
-								</>
-							) : (
-								<>
-									{/* <p className={styles.month_year}>{dateState.getDate()}</p>
-									<p className={styles.time}>
-										{dateState.toLocaleDateString('en', { month: 'long' })} {dateState.getFullYear()}
-									</p>
-									<p className={styles.month_year}>
-										{('0' + dateState.getHours()).slice(-2)} : {('0' + dateState.getMinutes()).slice(-2)} : 00
-									</p> */}
-								</>
-							)}
-							{/* <div className={styles.slogan}>
-								<div className={styles.text}>
-									{ReactHtmlParser(
-										idiom.content
-											.replace('rgb(0, 23, 55)', 'rgba(255, 255, 255, 0.9)')
-											.replace('rgb(0, 23, 55)', 'rgba(255, 255, 255, 0.9)')
-											.replace('rgb(0, 23, 55)', 'rgba(255, 255, 255, 0.9)')
-									)}
-								</div>
-							</div> */}
-						</div>
-					</div>
+					<img style={{ height: 300, width: 'auto' }} src="/images/login-top-icon.png" alt="trophy img" />
+					<img style={{ height: 500, width: 'auto' }} src="/images/login-bottom-icon.png" alt="trophy img" />
 				</div>
 				<div className={styles.content}>{children}</div>
 			</div>

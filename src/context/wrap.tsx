f4839b6b51605905c698358cd59d8d0cf0144376
longkntn2 <long.187pm13956@vanlanguni.vn>
2022-09-26 22:09:05 +0700

    fix bug

--- src/context/wrap.tsx
+++ src/context/wrap.tsx
@@ -147,14 +147,14 @@ const WrapProvider: FC<{ children: React.ReactNode }> = ({ children }) => {
 	}
 
 	const getAllData = () => {
-		if (userInfo) {
-			if (userInfo.RoleID === 1) {
-				getRoles(0)
-				getRoles(1)
-			}
-			getNewDataUser()
-			postOnesignalID()
-		}
+		// if (userInfo) {
+		// 	if (userInfo.RoleID === 1) {
+		getRoles(0)
+		getRoles(1)
+		// }
+		getNewDataUser()
+		postOnesignalID()
+		// }
 	}
 
 	useEffect(() => {
